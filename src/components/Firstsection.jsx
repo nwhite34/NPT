9f01a9f587c7161feb2ceb27516d4e6c465ce73e
nwhite34 <n.whiteley.34@student.scu.edu.au>
2024-04-22 17:39:20 +1000

    Using use effect to preload image

--- src/components/Firstsection.jsx
+++ src/components/Firstsection.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import nickJpg from '../nick.jpg'; // Adjust the relative path if needed
 
 const Firstsection = () => {
+  const [isImageLoaded, setImageLoaded] = useState(false);
+
+  useEffect(() => {
+    const img = new Image();
+    img.src = nickJpg;
+    img.onload = () => setImageLoaded(true);
+  }, []);
+
   return (
     <div
-      className="flex flex-col justify-center items-center h-screen w-full font-sans text-white"
+      className={`flex flex-col justify-center items-center h-screen w-full font-sans text-white ${!isImageLoaded ? 'invisible' : ''}`}
       style={{
         backgroundImage: `url(${nickJpg})`,
         backgroundSize: 'cover',
@@ -12,15 +20,19 @@ const Firstsection = () => {
         backgroundRepeat: 'no-repeat',
       }}
     >
-      <h1 className="text-4xl md:text-6xl font-bold text-center">
-        Take Your Fitness To The <span className="text-custom-gold"> Next Level </span> 
-      </h1>
-      <p className="text-xl text-center mt-4">
-     
-      </p>
-      <button className="mt-8 bg-transparent border border-white py-2 px-4 rounded-full text-white hover:bg-white hover:text-black transition duration-300 ease-in-out cursor-pointer">
-        START HERE
-      </button>
+      {isImageLoaded && (
+        <>
+          <h1 className="text-4xl md:text-6xl font-bold text-center">
+            Take Your Fitness To The <span className="text-custom-gold"> Next Level </span>
+          </h1>
+          <p className="text-xl text-center mt-4">
+            {/* Content */}
+          </p>
+          <button className="mt-8 bg-transparent border border-white py-2 px-4 rounded-full text-white hover:bg-white hover:text-black transition duration-300 ease-in-out cursor-pointer">
+            START HERE
+          </button>
+        </>
+      )}
     </div>
   );
 };
